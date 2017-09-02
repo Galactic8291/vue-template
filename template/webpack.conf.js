6e6bd6f92e9a5f92e759ab3083378de50806ba49
Robbie McClellan <robbiemcclellan14@gmail.com>
2017-09-02 15:49:07 -0400

    Configure webpack for index.js

--- template/webpack.conf.js
+++ template/webpack.conf.js
@@ -2,7 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-  entry: './src/main.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
