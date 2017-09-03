67c5ad0e38417b519e50b4f13ae72bff61e5529b
Robbie McClellan <robbiemcclellan14@gmail.com>
2017-09-02 20:36:46 -0400

    Add App.vue to template

---
+++ template/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id='app'>
+    {{#router}}
+    <router-view></router-view>
+    {{else}}
+    <Hello></Hello>
+    {{/router}}
+  </div>
+</template>
+
+<script>
+{{#unless router}}
+import Hello from './components/Hello'
+{{/unless}}
+
+export default {
+  name: 'app'{{#unless router}},
+  components: {
+    Hello
+  }{{/unless}}
+}
+</script>
+
+<style{{#less}} lang='less'{{/less}}>
+</style>
